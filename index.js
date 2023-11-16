86864c2dd7e434292fb35573ebb73ebf7136a29f
Nicole Park <nicolepark@bellsouth.net>
2023-11-16 15:29:15 -0500

    woked on coded

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 // import the pets array from data.js
-const pets = require('./data');
+const pets = require("./data");
 
 // init express app
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 8080;
 
 // GET - / - returns homepage
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + `/public/index.html`);
 });
 
 // hello world route
-app.get('/api', (req, res) => {
-    res.send('Hello World!');
+app.get("/api", (req, res) => {
+    res.send("Hello World!");
 });
 
 // get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
+app.get("/api/v1/pets", (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+app.get("/api/v1/pets/owner", (req, res) => {
     // get the owner from the request
-
+    const { owner } = req.query;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.filter((pet) => pet.owner.toLowerCase === owner);
 
     // send the pet as a response
-
+    res.send(pet.owner);
 });
 
 // get pet by name
-app.get('/api/v1/pets/:name', (req, res) => {
+app.get("/api/v1/pets/:name", (req, res) => {
     // get the name from the request
-
+    const name = req.params.name;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const pet = pets.find((pet) => pet.name.toLowerCase() === name.toLowerCase);
 
     // send the pet as a response
-
+    res.send(pets);
 });
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+    console.log("Server is listening on port " + PORT);
 });
 
 module.exports = app;
